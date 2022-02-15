1d4e238c831b223f3d18232e9f3d0089fdb1905e
vas_jramir <javier.ramirez@vasslatam.com>
2022-02-15 17:42:52 -0300

    se deja agregar al carro desde la lista

--- components/Amiibo.jsx
+++ components/Amiibo.jsx
@@ -2,26 +2,45 @@ import React from "react";
 import styles from '../styles/Amiibo.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
+import { connect } from "react-redux";
+import { addProductToCart } from "../actions/cart";
 
-const AmiiboList = ({amiibo}) => {
+const AmiiboList = (props) => {
     let randomPrice = Math.floor(Math.random() * 100) * 1000;
     let priceEncode = btoa(randomPrice);
-    let amiiboLink = '/amiibos/' + amiibo.head + amiibo.tail + '?p=' + priceEncode;
+    let amiiboLink = '/amiibos/' + props.amiibo.head + props.amiibo.tail + '?p=' + priceEncode;
+
     return(
         <li className={styles.amiicard}>
             <div className={styles.image}>
                 <Link href={amiiboLink}>
                     <div style={{ position: 'relative', width: '240px', height: '200px' }}>
-                        <Image layout="fill" objectFit="contain" src={amiibo.image} alt={amiibo.amiiboSeries} />
+                        <Image layout="fill" objectFit="contain" src={props.amiibo.image} alt={props.amiibo.amiiboSeries} />
                     </div>
                 </Link>
                 
             </div>
-            <h5>{amiibo.amiiboSeries}</h5>
+            <h5>{props.amiibo.amiiboSeries}</h5>
             <p className={styles.price}>$ {randomPrice.toLocaleString('es-CL')}</p>
-            <Link href={amiiboLink}><a className="papabtn">Ver producto</a></Link>
+            <button type="button" onClick={() => props.addProduct(props.amiibo, randomPrice)} className="papabtn">Agregar al carro</button>
+            <Link href={amiiboLink}><a>Ver producto</a></Link>
         </li>
     )
 }
 
-export default AmiiboList;
+const mapStateToProps = state => {
+    return {
+      cart: state.cart
+    };
+  };
+  
+  const mapDispatchToProps = dispatch => {
+    return {
+      addProduct: (amiibo, randomPrice) => dispatch(addProductToCart(amiibo, randomPrice))
+    };
+  };
+  
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(AmiiboList);
